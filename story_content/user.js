cc17dc468969d4422ca16f3d69dbe3fdeab58119
Sayasuryana14 <suryana.8236182004@mhs.unimed.ac.id>
2025-03-12 08:17:16 -0700

    Media Pembelajaran

---
+++ story_content/user.js
@@ -0,0 +1,91 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5jI2gZhTRvT":
+        Script1();
+        break;
+      case "6M4Ju5cu1z9":
+        Script2();
+        break;
+      case "5jhlUccUbzD":
+        Script3();
+        break;
+      case "6OfnRIARjwN":
+        Script4();
+        break;
+      case "6cV9hrUWtyv":
+        Script5();
+        break;
+      case "5dICpsE48oT":
+        Script6();
+        break;
+      case "6N3W3bRHQXJ":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById("bgSongku");
+audio.src="Musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=0.5;
+}
+
+function Script2()
+{
+  var audio = document.getElementById("bgSongku");
+audio.src="Musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=0.5;
+}
+
+function Script3()
+{
+  var audio = document.getElementById("bgSongku");
+audio.src="Musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=0.0;
+}
+
+function Script4()
+{
+  var audio = document.getElementById("bgSongku");
+audio.src="Musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=1.0;
+}
+
+function Script5()
+{
+  var audio = document.getElementById("bgSongku");
+audio.src="Musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=0.0;
+}
+
+function Script6()
+{
+  var audio = document.getElementById("bgSongku");
+audio.src="Musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=1.0;
+}
+
+function Script7()
+{
+  var audio = document.getElementById("bgSongku");
+audio.src="Musikku.mp3";
+audio.load();
+audio.play();
+audio.volume=0.0;
+}
+
